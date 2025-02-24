ede1196be79800ce22bbb942b2036e99dd7ac8b4
icomey8 <i.willcomey@gmail.com>
2025-02-23 20:21:48 -0800

    added sidebar toggle effect

--- Async/frontend/src/App.tsx
+++ Async/frontend/src/App.tsx
@@ -1,10 +1,10 @@
 import "./App.css";
-import TextEditor from "./components/TextEditor";
+// import TextEditor from "./components/TextEditor";
 import { Sidebar } from "@/components/Sidebar";
 import { Dashboard } from "@/components/Dashboard";
 import { Calendar } from "@/components/Calendar";
 // import { Courses } from "@/components/Courses";
-import { SidebarProvider } from "./components/ui/sidebar";
+import { SidebarProvider, SidebarTrigger } from "./components/ui/sidebar";
 
 function App() {
 	return (
@@ -12,12 +12,14 @@ function App() {
 			<SidebarProvider>
 				<Sidebar />
 				<main className="flex-1 overflow-y-auto p-6">
-					<h1 className="text-3xl font-bold mb-6">Welcome!</h1>
+					<div className="flex gap-2 items-center mb-3">
+						<SidebarTrigger />
+						<h1 className="text-3xl font-bold">Welcome!</h1>
+					</div>
 					<div className="grid gap-6">
 						<Dashboard />
 						{/* <Courses /> */}
 						<Calendar />
-						<TextEditor />
 					</div>
 				</main>
 			</SidebarProvider>
--- Async/frontend/src/routes/__root.tsx
+++ Async/frontend/src/routes/__root.tsx
@@ -4,10 +4,12 @@ import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 export const Route = createRootRoute({
 	component: () => (
 		<>
-			<div className="w-screen h-screen">
-				<Outlet />
+			<div className="flex justify-center w-screen ">
+				<div className="w-screen h-screen">
+					<Outlet />
+				</div>
+				<TanStackRouterDevtools />
 			</div>
-			<TanStackRouterDevtools />
 		</>
 	),
 });
